ba42ba1cb66eba6eb3ee2b1a74687929517268f5
okkyaaaaano <okkyaaaaano@gmail.com>
2020-10-26 09:34:41 +0000

    課題を追加

---
+++ kadai-02/index.js
@@ -0,0 +1,14 @@
+const firstName = 'Ayane'
+const familyName = 'Okano'
+console.log(firstName + familyName)  
+
+console.log(1+2+3+4+5+6+7+8+9+10)
+
+const dog = false
+const cat = true
+
+console.log(dog || cat)
+console.log(dog && cat)
+console.log(!dog && !cat)
+console.log(!(dog || cat))
+
