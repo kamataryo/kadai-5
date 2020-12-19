88c43f1fbd323332b39389bb91afb0b45cbfcb5b
okkyaaaaano <okkyaaaaano@gmail.com>
2020-12-19 07:48:12 +0000

    課題4を追加

---
+++ kadai-04/sample.js
@@ -0,0 +1,12 @@
+function MyFunction(name) {
+    console.log('Hello,' + name + '!')
+    console.log('Bye')
+}
+MyFunction('Aya')
+MyFunction('Okano')
+
+function add(n1 , n2){
+    console.log(n1 + n2)
+}
+
+add(10 , 100)
